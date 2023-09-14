2ec0c15944379570c30bdf8f6720797a40ad6945
张择怿 <zhangzeyi@shein.com>
2023-09-14 17:39:56 +0800

    feat: fizzy update

--- src/pages/demo-edit/Index.tsx
+++ src/pages/demo-edit/Index.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
-export default function Edit (): React.ReactElement<any, any> {
+export default function Edit(): React.ReactElement<any, any> {
   const { editId } = useParams();
+  const isEditType = editId !== null && editId !== undefined && editId !== "";
   // 有 editId 则为编辑，否则为新增
-  return (
-    <div>
-      {editId ? `EditID: ${editId}` : 'Add'}
-    </div>
-  )
+  return <div>{isEditType ? `EditID: ${editId}` : "Add"}</div>;
 }
